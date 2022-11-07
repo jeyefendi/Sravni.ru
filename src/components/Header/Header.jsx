f0e1de7cb2b08613a8d8a636fd58f42c2cdcba57
Jeyhun Farajov <97691925+jeyefendi@users.noreply.github.com>
2022-11-07 04:10:44 +0300

    Header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,27 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <NavLink to="/">
-        <img src="/logo.png" alt="Logo" className={styles.logo} />
-      </NavLink>
+      <section className={styles.nav}>
+        <NavLink to="/">
+          <img src="/logo.png" alt="Logo" className={styles.logo} />
+        </NavLink>
+        <div className={styles.navbar}>
+          <NavLink to="/">
+            <span className={styles.nav__links}>Home</span> 
+          </NavLink>
+          <NavLink to="/">
+            <span className={styles.nav__links}>Sign in</span> 
+          </NavLink>
+          <NavLink to="/">
+            <span className={styles.nav__links}>Sign up</span> 
+          </NavLink>
+        </div>
+      </section>
+
+      <section className={styles.banner}>
+        <h1>conduit</h1>
+        <p>A place share your knowledge.</p>
+      </section>
     </header>
   );
 };
